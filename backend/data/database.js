60f2084881bfe1dbe68d837b3666c1ce1b245dab
Jakov Svalina <svalina.jakov@gmail.com>
2025-11-01 23:21:15 +0100

    Add PostgreSQL connection using pg Pool in database.js
    Introduced a PostgreSQL connection using the pg library's Pool in addition to the existing Supabase client. Added a connectClient function to test the database connection and log the result.

--- backend/data/database.js
+++ backend/data/database.js
@@ -1,2 +1,28 @@
-import { createClient } from '@supabase/supabase-js';
-export const db = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+import { createClient } from "@supabase/supabase-js";
+export const db = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+const { Client, Pool } = require("pg");
+
+const pool = new Pool({
+    user: "placeholder",
+    host: "placeholder",
+    database: "placeholder",
+    password: "placeholder",
+    port: "placeholder",
+});
+
+async function connectClient() {
+    const client = await pool.connect();
+    try {
+        console.log("Successful connection to db.");
+    } catch (error) {
+        console.error("Connection error to db.");
+    } finally {
+        client.release();
+    }
+}
+
+connectClient();
