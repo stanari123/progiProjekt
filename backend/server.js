7eed23a5092f020213f91103d6c000214344111a
LukaZ-25 <luka.zoric@fer.hr>
2026-01-10 13:16:23 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ app.get("/health", (_req, res) => {
 
 // za React
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
